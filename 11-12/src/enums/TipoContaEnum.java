37fbcffc3f9f3fbacfc31deb4aa5ed19d50fcf64
t1sbita <elias.sgarcia@gmail.com>
2020-12-14 15:58:27 -0300

    Aula dia 11 refeita às pressas

---
+++ 11-12/src/enums/TipoContaEnum.java
@@ -0,0 +1,9 @@
+package enums;
+
+public enum TipoContaEnum
+{
+	POUPANCA,
+	CORRENTE;
+			
+	
+}
---
+++ 11-12/src/models/Pessoas.java
@@ -0,0 +1,28 @@
+package models;
+
+public class Pessoas
+{
+	private String NomeCliente;
+	private String Cpf;
+	
+	public String getNomeCliente()
+	{
+		return NomeCliente;
+	}
+	
+	public void setNomeCliente(String nomeCliente)
+	{
+		NomeCliente = nomeCliente;
+	}
+	
+	public String getCpf()
+	{
+		return Cpf;
+	}
+	
+	public void setCpf(String cpf)
+	{
+		Cpf = cpf;
+	}
+	
+}
